41f4783d3ac0878e803e5cd1981882dfdfd5d72d
mohyehia <mohammedyehia99@gmail.com>
2021-03-05 22:23:54 +0200

    adding another function to count the number of 1 bits in binary representation of number

--- src/main/java/com/mohyehia/algo/bits/NumberOfOnesInBinaryRepresentation.java
+++ src/main/java/com/mohyehia/algo/bits/NumberOfOnesInBinaryRepresentation.java
@@ -88,4 +88,16 @@ public class NumberOfOnesInBinaryRepresentation {
         }
         return count;
     }
+
+    /*
+    Another solution for counting the number of 1's in binary representation of number
+    Works for positive & negative number
+     */
+    static int anotherCountBitSet(int n){
+        int cnt = 0;
+        for(int i = 0; i < 32; i++){
+            if((n & 1) != 0) cnt++;
+        }
+        return cnt;
+    }
 }
