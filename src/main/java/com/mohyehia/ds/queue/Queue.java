d3648c1578c2f13e6c5f717f0aace71a7213723e
Mohammed Yehia <mohammedyehia99@gmail.com>
2023-02-17 17:07:38 +0200

    Adding iterativeBinarySearch functionality

--- src/main/java/com/mohyehia/ds/queue/Queue.java
+++
@@ -1,9 +0,0 @@
-package com.mohyehia.ds.queue;
-
-public interface Queue {
-    void enqueue(int val);
-    int dequeue();
-    int peek();
-    int size();
-    boolean isEmpty();
-}
--- src/main/java/com/mohyehia/ds/stack/Stack.java
+++
@@ -1,9 +0,0 @@
-package com.mohyehia.ds.stack;
-
-public interface Stack {
-    void push(int val);
-    int pop();
-    int peek();
-    int size();
-    boolean isEmpty();
-}
